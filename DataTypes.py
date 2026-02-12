4b8086ba057f28c76873c65d18880787aec1197d
3ltnz14-gif <3ltnz14@gmail.com>
2026-02-12 19:26:32 +1300

    Add files via upload

---
+++ DataTypes.py
@@ -0,0 +1,11 @@
+a = 5
+print("The variable a is aa", type(a))
+
+b = 2.5
+print("The variable b is a", type(b))
+
+c = "coding"
+print("The variable c is a", type(c))
+
+d = True
+print("The variable d is a", type(d))
