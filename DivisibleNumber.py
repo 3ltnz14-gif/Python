1ed09bd10dd74fb4dc61867c46fd8a28df09e796
3ltnz14-gif <3ltnz14@gmail.com>
2026-02-24 18:47:07 +1300

    Add files via upload

---
+++ DivisibleNumber.py
@@ -0,0 +1,7 @@
+numn = int(input("Enter a number (Numerator): "))
+numd = int(input("Enter a number (Denominator):0"))
+
+if numn % numd == 0:
+    print("/n {0} is divisible by {1}." .format(str(numn), str(numd)))
+else:
+    print("/n {0} is not divisible by {1}." .format(str(numn), str(numd)))
