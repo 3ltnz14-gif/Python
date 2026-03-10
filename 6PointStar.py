6520717d29ceed4aebb3fd7b84d03d33fd7fa3dd
3ltnz14-gif <3ltnz14@gmail.com>
2026-03-10 18:58:09 +1300

    Add files via upload

---
+++ 6PointStar.py
@@ -0,0 +1,27 @@
+import turtle
+turtle.Screen().bgcolor("Aqua")
+pen = turtle.Turtle()
+
+pen.forward(120)
+
+pen.left(120)
+pen.forward(120)
+
+pen.left(120)
+pen.forward(120)
+
+pen.penup()
+pen.right(150)
+pen.forward(60)
+
+pen.pendown()
+pen.right(90)
+pen.forward(120)
+
+pen.right(120)
+pen.forward(120)
+
+pen.right(120)
+pen.forward(120)
+
+turtle.done()
