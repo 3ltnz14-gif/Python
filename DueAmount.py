7e4dce9163ed15283c9a64188ac0b2e26613a25e
3ltnz14-gif <3ltnz14@gmail.com>
2026-03-17 18:54:50 +1300

    Add files via upload

--- DueAmount.py
+++ DueAmount.py
@@ -1,6 +1,6 @@
 total = int(input("How much was the total? "))
 paid = int(input("How much was already paid? "))
-def due():
-    return print("The customer still needs to pay $", total-paid)
+def due(x, y):
+    return print("The customer still needs to pay $", x-y)
 
-due()
+due(total, paid)
