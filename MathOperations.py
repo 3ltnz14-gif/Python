6664d4c50c9136acb0a1c350ad9c7cbc672bb993
3ltnz14-gif <3ltnz14@gmail.com>
2026-03-19 19:55:38 +1300

    Add files via upload

---
+++ MathOperations.py
@@ -0,0 +1,11 @@
+import math
+print("The floor and ceiling of 23.56 are:", str(math.ceil(23.56)), "and", str(math.floor(23.56)))
+
+x = 10
+y = -15
+
+print("The value of x after copying the sign of y is:", str(math.copysign(x, y)))
+
+print("Absolute value of -96 and 56 are:", str(math.fabs(56)), "and", str(math.fabs(56)))
+
+print("The HCF of 24 and 56 is: ", str(math.gcd(24, 56)))
