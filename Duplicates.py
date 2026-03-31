27151be279dd7c6de482f6d219a2667c3f7e5817
3ltnz14-gif <3ltnz14@gmail.com>
2026-03-31 18:57:41 +1300

    Add files via upload

---
+++ Duplicates.py
@@ -0,0 +1,16 @@
+s_data = {"id1": {"name":"Sara", "class":"V", "subject_integration":"english, math, science"},
+          "id2": {"name":"David", "class":"V", "subject_integration":"english, math, science"},
+          "id3": {"name":"Sara", "class":"V", "subject_integration":"english, math, science"},
+          "id4": {"name":"Surya", "class":"V", "subject_integration":"english, math, science"},}
+
+result = {}
+seen_keys = []
+for s_id, details in s_data.items():
+    unique_key = (details["name"], details["class"], details["subject_integration"])
+
+    if unique_key not in seen_keys:
+        seen_keys.append(unique_key)
+        result[s_id] = details
+
+for k, val in result.items():
+    print(k, ":", val)
