9d1cfcb4f52c0a38690325d1079ac5e89a3f643a
3ltnz14-gif <3ltnz14@gmail.com>
2026-04-09 18:28:53 +1200

    Add files via upload

---
+++ StringUpperCase.py
@@ -0,0 +1,13 @@
+class iostring():
+    def __init__(self):
+        self.str1 = ""
+
+
+string = iostring()
+def getstring(self):
+    self.str1 = input("Enter a string: ")
+def printstring(self):
+    self.str1 = self.str1.upper()
+    print(self.str1)
+getstring(string)
+printstring(string)
