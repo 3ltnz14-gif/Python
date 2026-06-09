492586b1d7a5c6b4ff9f4a256fff54193e6114d1
3ltnz14-gif <3ltnz14@gmail.com>
2026-06-09 19:36:49 +1200

    Add files via upload

--- LCM.py
+++ LCM.py
@@ -1,13 +1,28 @@
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
+oneortwo = int(input("Would you like to do the first or second way? (1 or 2)"))
 
-if a > b:
-    highest = a
+if oneortwo != 1 and oneortwo != 2:
+    print("Invalid input")
+elif oneortwo == 1:
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+
+    if a > b:
+        highest = a
+    else:
+        highest = b
+    print(f"LCM of {a} and {b}:")
+    for i in range(highest, (a*b) + 1):
+        if i % a == 0 and i % b == 0:
+            print(f">{i}")
+            break
 else:
-    highest = b
-print(f"LCM of {a} and {b}:")
-for i in range(highest, (a*b) + 1):
-    if i % a == 0 and i % b == 0:
-        print(f">{i}")
-        break
-
+    def hcf(smallest, largest):
+        while smallest:
+            store = smallest
+            smallest = largest%smallest
+            largest = store
+        return largest
+    largest = int(input("Enter the greater number:"))
+    smallest = int(input("Enter the lesser number:"))
+    lcm = int((smallest / hcf(smallest, largest))*largest)
+    print("LCM is", lcm)
