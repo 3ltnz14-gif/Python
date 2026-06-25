8f783a99910d8719525a86a6a1f19ab427b7ae28
3ltnz14-gif <3ltnz14@gmail.com>
2026-06-25 18:48:00 +1200

    Add files via upload

---
+++ BinaryClueInvestigator.py
@@ -0,0 +1,10 @@
+def reversebits(number):
+    reversed = 0
+    while number > 0:
+        reversed = reversed << 1
+        if number & 1 == 1:
+            reversed = reversed ^ 1
+        number = number >> 1
+    return reversed
+number = int(input("Enter your number: "))
+print(f"Number with reversed bits: {reversebits(number)}")
